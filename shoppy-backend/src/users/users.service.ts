edb0e0df886a04e7af5c251f95caff586f01f8bd
Green00101 <2585640230@qq.com>
2024-11-23 20:16:00 +1100

    start with auth

--- shoppy-backend/src/users/users.service.ts
+++ shoppy-backend/src/users/users.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { CreateUserRequest } from './dto/create-user.request';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { User } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
+
   async createUser(data: CreateUserRequest) {
     try {
       return await this.prismaService.user.create({
@@ -15,8 +16,8 @@ export class UsersService {
           password: await bcrypt.hash(data.password, 10),
         },
         select: {
-          id: true,
           email: true,
+          id: true,
         },
       });
     } catch (err) {
@@ -25,5 +26,11 @@ export class UsersService {
       }
       throw err;
     }
+  }
+
+  async getUser(filter: Prisma.UserWhereUniqueInput) {
+    return this.prismaService.user.findUniqueOrThrow({
+      where: filter,
+    });
   }
 }
